7b70b1984803728afe33ddb566a8c310b0efbfc2
Andrew Oberstar <ajoberstar@gmail.com>
2015-04-03 15:01:02 -0500

    Working clock in Om.

--- src/core.cljs
+++ src/core.cljs
@@ -1,1 +1,32 @@
-(ns org.ajoberstar.lightdm.core)
+(ns org.ajoberstar.lightdm.core
+  (:require [om.core :as om]
+            [om.dom :as dom]))
+
+(enable-console-print!)
+
+(def clock-state (atom {}))
+
+(defn- tick []
+  (swap! clock-state assoc :time (js/Date.)))
+
+(defn clock-view [{:keys [time interval-id]} _]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (let [new-id (js/setInterval tick 1000)]
+        (swap! clock-state assoc :interval-id new-id)))
+    om/IWillUnmount
+    (will-unmount [_]
+      (js/clearInterval interval-id))
+    om/IRender
+    (render [_]
+      (dom/span nil (str time)))))
+
+(om/root clock-view
+         clock-state
+         {:target (. js/document (getElementById "time"))})
+
+
+(def user-state (atom {:active-user nil
+                       :users []}))
+
