62ca6b8529f3cb15688f7e8ccf43b108c8b2cbfb
Andrew Oberstar <ajoberstar@gmail.com>
2015-04-11 15:06:06 -0500

    Added sessions component.
    Had to remove the left floating due to a chromium bug:
    https://code.google.com/p/chromium/issues/detail?id=7423

--- src/org/ajoberstar/lightdm.cljs
+++ src/org/ajoberstar/lightdm.cljs
@@ -6,6 +6,9 @@
 
 (enable-console-print!)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Clock component
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (def clock-format (format/formatter "yyyy-MM-dd hh:mm:ss A"))
 
 (defn clock-component []
@@ -15,3 +18,26 @@
       [:span (format/unparse clock-format @clock)])))
 
 (reagent/render [clock-component] (js/document.getElementById "time"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Session component
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(let [sessions (-> (. js/lightdm -sessions) (js->clj :keywordize-keys true))
+      active (-> (. js/lightdm -default-session) (js->clj :keywordize-keys true))]
+  (def sessions-db (reagent/atom {:active-session active
+                                  :sessions sessions})))
+
+(defn session-component []
+  (let [{:keys [active-session sessions]} @sessions-db
+        active-key (:key active-session)]
+    [:select
+     {:defaultValue active-key}
+     (map (fn [{:keys [key name]}]
+            ^{:key key} [:option {:value key} name])
+          sessions)]))
+
+(reagent/render [session-component] (js/document.getElementById "sessions"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; User component
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
